39b2f680a872b256819616661ff693302295337a
Katchietsnouva <philipaswa01@gmail.com>
2023-09-22 15:12:51 +0300

    3rd Commit
    GUI + Arduino code Interface v1

--- Arduino_GUI_v1/Form1.Designer.cs
+++ Arduino_GUI_v1/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.onButton = new System.Windows.Forms.Button();
             this.offButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -50,7 +51,7 @@
             // 
             // offButton
             // 
-            this.offButton.Location = new System.Drawing.Point(106, 38);
+            this.offButton.Location = new System.Drawing.Point(115, 38);
             this.offButton.Name = "offButton";
             this.offButton.Size = new System.Drawing.Size(75, 23);
             this.offButton.TabIndex = 1;
@@ -58,16 +59,28 @@
             this.offButton.UseVisualStyleBackColor = true;
             this.offButton.Click += new System.EventHandler(this.offButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connect via COM3 ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.offButton);
             this.Controls.Add(this.onButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NodeMCU IoT GUI";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +89,7 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button onButton;
         private System.Windows.Forms.Button offButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
