9a928486d47f2b838ce5a569b84e10a234b190b6
Katchietsnouva <philipaswa01@gmail.com>
2023-09-22 15:15:31 +0300

    Update Arduino_Code.ino
    Ch

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -20,28 +20,3 @@ void loop() {
   }
 }
 
-
-
-// String data;
-// char d1;
-// void setup() {
-//   // put your setup code here, to run once:
-//   Serial.begin(9600 );
-//   pinMode(13, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
-
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   if (Serial.available()){
-//     data = Serial.readString();
-//     d1 = data.charAt(0);
-//     if (d1=='A'){
-//       digitalWrite(13, HIGH);
-//     }
-//     else if (d1 == 'a'){
-//       digitalWrite(13, LOW);
-//     }
-//   }
-
-// }
